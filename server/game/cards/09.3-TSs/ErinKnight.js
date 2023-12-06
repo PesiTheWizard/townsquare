41dca2624a18679c1ff42cf73e676f4ad2d9c42a
PesiTheWizard <rigningin@simnet.is>
2023-12-06 11:15:09 +0000

    Implement ErinKnight.js
    Boots the joiner as a cost

--- server/game/cards/09.3-TSs/ErinKnight.js
+++ server/game/cards/09.3-TSs/ErinKnight.js
@@ -1,9 +1,28 @@
-class DummyCard {
-    setupCardAbilities() {
+const DudeCard = require('../../dudecard.js');
+const GameActions = require('../../GameActions');
 
+class ErinKnight extends DudeCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Shootout: Erin Knight',
+            playType: ['shootout:join'],
+            cost: ability.costs.boot(card => card.location === 'play area' &&
+                card.getType() === 'dude' &&
+                card.controller.equals(this.controller) &&
+                card.hasKeyword('deputy') && !card.isParticipating()),
+            condition: () => this.isParticipating(),
+            repeatable: true,
+            message: context => this.game.addMessage('{0} uses {1} to move {2} into the posse booted',
+                context.player, this, context.costs.boot),
+            handler: context => {
+                this.game.resolveGameAction(GameActions.joinPosse({
+                    card: context.costs.boot
+                }), context);
+            }
+        });
     }
 }
 
-DummyCard.code = '';
+ErinKnight.code = '17006';
 
-module.exports = DummyCard;
+module.exports = ErinKnight;
