4a81d3bf8c44c590e74c223ef02ff78160b3e68b
PesiTheWizard <rigningin@simnet.is>
2023-12-06 11:24:04 +0000

    Implement ErinKnight.js
    Targets the joining dude

--- server/game/cards/09.3-TSs/ErinKnight.js
+++ server/game/cards/09.3-TSs/ErinKnight.js
@@ -1,9 +1,36 @@
-class DummyCard {
-    setupCardAbilities() {
+const DudeCard = require('../../dudecard.js');
+const GameActions = require('../../GameActions');
 
+class ErinKnight extends DudeCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Shootout: Erin Knight',
+            playType: ['shootout:join'],
+            target: {
+                cardCondition: {
+                    location: 'play area',
+                    participating: false,
+                    booted: false,
+                    controller: 'current'
+                    condition: card => card.hasKeyword('deputy')
+                },
+                cardType: 'dude',
+                gameAction: ['joinPosse', 'boot']
+            }
+            condition: () => this.isParticipating(),
+            repeatable: true,
+            message: context => this.game.addMessage('{0} uses {1} to move {2} into the posse booted',
+                context.player, this, context.target),
+            handler: context => {
+                this.game.resolveGameAction(GameActions.joinPosse({
+                    card: context.target,
+                    options: { needToBoot: true }
+                }), context);
+            }
+        });
     }
 }
 
-DummyCard.code = '';
+ErinKnight.code = '17006';
 
-module.exports = DummyCard;
+module.exports = ErinKnight;
