9d33a4bc28d3db38b329fcec6a99a261607a81f9
PesiTheWizard <rigningin@simnet.is>
2024-01-25 01:11:45 +0000

    Update AngelaPayne.js

--- server/game/cards/02.3-EDS/AngelaPayne.js
+++ server/game/cards/02.3-EDS/AngelaPayne.js
@@ -6,13 +6,14 @@ class AngelaPayne extends DudeCard {
             title: 'Gain 2 Ghost Rock',
             playType: 'noon',
             cost: ability.costs.bootSelf(),
-            ifCondition: () => this.locationCard.owner !== this.controller && this.locationCard.getType() === 'deed',
-            ifFailMessage: context => this.game.addMessage('{0} boots {1} but does not gain any GR because she is not at a deed {0} doesn\'t own', context.player, this),
+            ifCondition: () => !this.locationCard.owner.equals(this.controller) && this.locationCard.getType() === 'deed',
+            ifFailMessage: context => this.game.addMessage('{0} boots {1} but does not gain any GR because she is not at a deed {0} doesn\'t own',
+                context.player, this),
             message: context => this.game.addMessage('{0} boots {1} to gain 2 GR', context.player, this),
             handler: () => {
                 this.controller.modifyGhostRock(2);
             }
-        });    
+        });
     }
 }
 
