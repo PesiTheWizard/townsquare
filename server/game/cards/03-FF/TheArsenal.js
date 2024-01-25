3806256dcb0e38d5d8a1c11e0ae46111a07c05e3
PesiTheWizard <rigningin@simnet.is>
2024-01-25 01:30:32 +0000

    Update TheArsenal.js

--- server/game/cards/03-FF/TheArsenal.js
+++ server/game/cards/03-FF/TheArsenal.js
@@ -10,17 +10,17 @@ class TheArsenal extends OutfitCard {
             cost: ability.costs.boot(card =>
                 card.location === 'play area' &&
                 card.parent &&
-                card.controller === this.owner &&
+                card.controller.equals(this.owner) &&
                 (card.isGadget() || card.isSpell())
             ),
-            message: context => 
+            message: context =>
                 this.game.addMessage('{0} uses {1} to call out {2}', context.player, this, context.target),
             handler: context => {
                 context.ability.selectAnotherTarget(context.player, context, {
                     activePromptTitle: 'Select dude to Call Out',
                     waitingPromptTitle: 'Waiting for opponent to select dude',
                     cardCondition: card => card.location === 'play area' && 
-                        card.controller !== context.player &&
+                        !card.controller.equals(context.player) &&
                         card.getType() === 'dude' &&
                         card.gamelocation === context.costs.boot.gamelocation &&
                         (card.isWanted() || !card.booted),
@@ -28,13 +28,13 @@ class TheArsenal extends OutfitCard {
                     gameAction: 'callout',
                     onSelect: (player, callee) => {
                         this.game.resolveGameAction(GameActions.callOut({ caller: context.costs.boot.parent, callee }), context).thenExecute(() => {
-                            this.game.addMessage('{0} uses {1} to have {2} call out {3}', 
-                                player, this, context.costs.boot.parent, callee);  
+                            this.game.addMessage('{0} uses {1} to have {2} call out {3}',
+                                player, this, context.costs.boot.parent, callee);
                         });
                         return true;
                     },
                     source: this
-                });                
+                });
             }
         });
     }
