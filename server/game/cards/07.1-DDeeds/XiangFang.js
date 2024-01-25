27b9da7ca5efab6e21c7b5202882543ca567009f
PesiTheWizard <rigningin@simnet.is>
2024-01-25 02:26:49 +0000

    Update XiangFang.js

--- server/game/cards/07.1-DDeeds/XiangFang.js
+++ server/game/cards/07.1-DDeeds/XiangFang.js
@@ -7,18 +7,18 @@ class XiangFang extends DudeCard {
             when: {
                 onCardEntersPlay: event => event.card === this
             },
-            message: context => 
-                this.game.addMessage('{0} gains {1} GR thanks to the {2}', 
+            message: context =>
+                this.game.addMessage('{0} gains {1} GR thanks to the {2}',
                     context.player, this.locationCard.production, this),
             handler: context => {
                 context.player.modifyGhostRock(this.locationCard.production);
-                if(this.isAtHome || (this.locationCard.getType() === 'deed' && this.locationCard.owner !== context.player)) {
+                if(this.isAtHome || (this.locationCard.getType() === 'deed' && !this.locationCard.owner.equals(context.player))) {
                     this.untilEndOfPhase(context.ability, ability => ({
                         match: this.locationCard,
                         effect: ability.effects.setProduction(0)
                     }), PhaseNames.Upkeep
                     );
-                    this.game.addMessage('{0} uses {1} to set {2}\'s production to 0', 
+                    this.game.addMessage('{0} uses {1} to set {2}\'s production to 0',
                         context.player, this, this.locationCard);
                 }
             }
