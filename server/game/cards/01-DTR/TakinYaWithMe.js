0f5fcb4ba578e44f472148b96bded149c4d481f6
PesiTheWizard <rigningin@simnet.is>
2024-01-25 00:55:06 +0000

    Update TakinYaWithMe.js

--- server/game/cards/01-DTR/TakinYaWithMe.js
+++ server/game/cards/01-DTR/TakinYaWithMe.js
@@ -7,13 +7,13 @@ class TakinYaWithMe extends ActionCard {
         this.reaction({
             title: 'React: Takin\' Ya With Me',
             when: {
-                onTakenCasualties: event => event.shootout.loser === this.controller && 
-                    event.player === this.controller &&
+                onTakenCasualties: event => event.shootout.loser.equals(this.controller) && 
+                    event.player.equals(this.controller) &&
                     this.canBeTakenWithMe(event.player, event.shootout, event.casualtiesTaken)
             },
             handler: context => {
-                this.game.resolveGameAction(GameActions.increaseCasualties({ 
-                    player: context.player.getOpponent(), 
+                this.game.resolveGameAction(GameActions.increaseCasualties({
+                    player: context.player.getOpponent(),
                     amount: 1
                 }), context).thenExecute(() => {
                     this.game.addMessage('{0} uses {1} to take some of {2}\'s dudes or sidekicks with them', context.player, this, context.player.getOpponent());
@@ -27,7 +27,6 @@ class TakinYaWithMe extends ActionCard {
         let opponentPosseDudes = shootout.getPosseByPlayer(player.getOpponent()).getDudes();
         const reducer = (highest, current) => current > highest ? current : highest;
         let highestBullets = takenCasualties.map(casualty => casualty.bullets || 0).reduce(reducer, 0);
-        
         return opponentPosseDudes.some(dude => dude.bullets <= highestBullets);
     }
 }
