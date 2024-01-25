b6f0e2ad4db6157b5b8ac1e82a777cbc66e0de82
PesiTheWizard <rigningin@simnet.is>
2024-01-25 01:22:14 +0000

    Update FasterOnTheDraw.js

--- server/game/cards/03-FF/FasterOnTheDraw.js
+++ server/game/cards/03-FF/FasterOnTheDraw.js
@@ -26,15 +26,15 @@ class FasterOnTheDraw extends ActionCard {
                 context.ability.selectAnotherTarget(context.player, context, {
                     activePromptTitle: 'Select an opposing dude who is slower on the draw',
                     waitingPromptTitle: 'Waiting for opponent to select dude',
-                    cardCondition: card => card.isParticipating() && card.controller !== context.player,
+                    cardCondition: card => card.isParticipating() && !card.controller.equals(context.player),
                     cardType: 'dude',
                     onSelect: (player, card) => {
                         this.applyAbilityEffect(context.ability, ability => ({
                             match: card,
                             effect: ability.effects.modifyBullets(-2)
                         }));
-                        this.game.addMessage('{0} uses {1} to give {2} +1 bullets {3} and to give {4} -2 bullets', 
-                            context.player, this, context.target, deputyBonus, card);                     
+                        this.game.addMessage('{0} uses {1} to give {2} +1 bullets {3} and to give {4} -2 bullets',
+                            context.player, this, context.target, deputyBonus, card);
                         return true;
                     },
                     source: this
