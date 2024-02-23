067c2836dea7cf7d567f07c7d732ee1df7ab7465
PesiTheWizard <rigningin@simnet.is>
2024-02-23 16:47:06 +0000

    Update EagleWardens.js
    Fixing my mess

--- server/game/cards/05-IOUF/EagleWardens.js
+++ server/game/cards/05-IOUF/EagleWardens.js
@@ -19,7 +19,7 @@ class EagleWardens extends OutfitCard {
                 this.abilityContext = context;
                 let cardsToDraw = context.costs.boot.influence < 2 ? 2 : 3;
                 context.player.drawCardsToHand(cardsToDraw, context).thenExecute(() => {
-                    this.game.addMessage('{0} uses {1} and boots {2} to draw {3} cards',
+                    this.game.addMessage('{0} uses {1} and boots {2} to draw {3} cards', 
                         context.player, this, context.costs.boot, cardsToDraw);
                     this.game.promptForSelect(context.player, {
                         activePromptTitle: 'Select a card',
