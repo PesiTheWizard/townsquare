4f36a6f5bda5258169c20fadf779b58adedd945c
PesiTheWizard <rigningin@simnet.is>
2024-02-23 17:09:27 +0000

    Update TownCouncil.js
    Fixing my mess

--- server/game/cards/02.1-NTNR/TownCouncil.js
+++ server/game/cards/02.1-NTNR/TownCouncil.js
@@ -13,8 +13,8 @@ class TownCouncil extends DeedCard {
                 cardCondition: { location: 'play area', controller: 'current' },
                 cardType: ['dude']
             },
-            message: context =>
-                this.game.addMessage('{0} uses {1} to give {2} +3 influence until after next Noon play',
+            message: context => 
+                this.game.addMessage('{0} uses {1} to give {2} +3 influence until after next Noon play', 
                     context.player, this, context.target),
             handler: context => {
                 this.lastingEffect(context.ability, ability => ({
